9e1f4536f7a213be07c63f0ab5c30fe474d54af5
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-07-24 18:28:00 +0900

    Initial commit

---
+++ src/main/java/com/memewiki/core/config/HtmlCharacterEscapes.java
@@ -0,0 +1,37 @@
+package com.memewiki.core.config;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class HtmlCharacterEscapes extends CharacterEscapes {
+    private final int[] asciiEscapes;
+
+    public HtmlCharacterEscapes() {
+        // 1. XSS 방지 처리할 특수 문자 지정
+        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+    }
+
+    @Override
+    public int[] getEscapeCodesForAscii() {
+        return asciiEscapes;
+    }
+
+    @Override
+    public SerializableString getEscapeSequence(int ch) {
+        char charAt = (char) ch;
+        if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+            String sb = "\\u" + String.format("%04x", ch);
+            return new SerializedString(sb);
+        }
+        return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+    }
+}
