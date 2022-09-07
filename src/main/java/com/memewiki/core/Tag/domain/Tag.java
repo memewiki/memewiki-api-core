d8cf466651e58b238f77af4cad830e7ca9a2d39b
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-09-07 19:54:16 +0900

    ✨ 기본 도메인 세팅
    - 기본 도메인 세팅

---
+++ src/main/java/com/memewiki/core/Tag/domain/Tag.java
@@ -0,0 +1,26 @@
+package com.memewiki.core.Tag.domain;
+
+import com.memewiki.core.common.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Tag extends BaseEntity {
+    @Id
+    @Column(name = "tag")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tagName;
+
+}
