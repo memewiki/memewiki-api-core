ee3a3462712432cfeb5b3d3f43aafb07640d8b8f
RyooChan <53744363+RyooChan@users.noreply.github.com>
2023-01-17 23:47:11 +0900

    ✨[meme] : 밈 상세내용 확인 기능 구현
    - 밈 상세내용 확인 기능 구현
        - 상세 확인 시 조회수(hit) update기능 추가
    - 밈 저장 코드 리팩터링
        - save -> saveAll 활용
            - saveALl 활용을 통한 속도 개선 진행

--- src/main/java/com/memewiki/core/domain/memeTag/repository/MemeTagRepository.java
+++ src/main/java/com/memewiki/core/domain/memeTag/repository/MemeTagRepository.java
@@ -4,6 +4,9 @@ import com.memewiki.core.domain.memeTag.domain.MemeTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemeTagRepository extends JpaRepository<MemeTag, Long> {
+    List<MemeTag> findAllByMemeId(Long memeId);
 }
